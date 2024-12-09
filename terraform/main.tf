aa814ff27963660260467bb9ce69efa57c376767
joseimanul <josem123321@gmail.com>
2024-12-09 12:53:13 -0500

    Launching

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_version = "~> 1.3"
   backend "azurerm" {
-    
+
   }
 
   required_providers {
     azapi = {
-      source = "Azure/azapi"
+      source  = "Azure/azapi"
       version = "~> 1.15"
     }
 
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.75"
     }
   }
