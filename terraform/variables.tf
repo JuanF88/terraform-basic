c79733483feb9ea38332f0ab11d241f9273538f1
joseimanul <josem123321@gmail.com>
2024-12-09 10:17:53 -0500

    testing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,3 +23,34 @@ variable "resource_groups" {
     tags            = optional(map(any), {})
   }))
 }
+
+variable "bounded_context" {
+  type = map(string)
+}
+
+variable "domain" {
+  description = "The name of the domain for these workloads."
+  type        = string
+}
+
+variable "environment" {
+  description = "Name of the deployment promotion environment"
+  type        = string
+
+  validation {
+    condition     = var.environment == "dev" || var.environment == "qa" || var.environment == "uat" || var.environment == "prod" || var.environment == "dr"
+    error_message = "Environment must be dev, qa, uat, dr, or prod."
+  }
+}
+
+variable "location" {
+  description = "The Azure region that will be the primary location for resource creation."
+  type        = string
+  default     = "eastus"
+}
+
+variable "iterator" {
+  type    = string
+  default = ""
+}
+
