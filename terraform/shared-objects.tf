c2b7d21fa3c5a76984e39350e9ee71d8ecf52dc3
joseimanul <josem123321@gmail.com>
2024-12-09 11:30:34 -0500

    Launching

--- terraform/shared-objects.tf
+++ terraform/shared-objects.tf
@@ -1,4 +1,4 @@
-module "shared_objects" {
+module "shared-objects" {
   source           = "git::https://github.com/JuanF88/terraform-modules//azurerm-modules-shared-objects?ref=shared-objects-v1.0.0"
   location_primary = var.location
   environment      = var.environment
